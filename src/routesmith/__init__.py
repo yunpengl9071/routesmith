8c8eba617a74dee92a3a4750e9d34d5cf41796eb
Yunpeng Liu-Lupo <yunpeng.liu9071@gmail.com>
2026-07-16 00:49:54 -0400

    chore: bump __version__ to 0.9.0 for PyPI publish

--- src/routesmith/__init__.py
+++ src/routesmith/__init__.py
@@ -27,7 +27,7 @@ from routesmith.predictor.linucb import LinUCBPredictor
 from routesmith.registry.models import ModelConfig, ModelRegistry
 from routesmith.strategy.ab_test import ABTestRunner
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 
 __all__ = [
     "ABTestRunner",
